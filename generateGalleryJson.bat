fb320dc1769ea4e48abaa1a554a7eeea7654c467
ebibibo <edobibeau@hotmail.com>
2026-04-06 12:00:39 -0400

    gallery

--- generateGalleryJson.bat
+++ generateGalleryJson.bat
@@ -1,6 +1,22 @@
 @echo off
-REM Run the Node script to update gallery.json
+REM 1. Generate gallery.json
+echo Updating gallery...
 node generateGalleryJson.js
 
-REM Pause so you can see the output
+REM 2. Stage changes in Git
+echo Adding files to Git...
+git add gallery.json
+
+REM 3. Commit changes
+set /p commitMessage=Enter commit message: 
+git commit -m "%commitMessage%"
+
+REM 4. Push to GitHub
+echo Pushing to GitHub...
+git push
+
+REM 5. Open portfolio in browser
+echo Opening portfolio in browser...
+start "" "index.html"
+
 pause
